19dd6e008264961bd5b41f989b5c129c3972fbbe
Malachi Dewitt <dewitt851@gmail.com>
2024-07-27 22:27:15 -0400

    Integrated RAG

--- Chatbot.py
+++ Chatbot.py
@@ -27,11 +27,14 @@ def configure_model(bedrock_client, foundation_model="anthropic.claude-instant-v
 
 def start_conversation(llm, patient_name) -> None:
     """ Starts the Conversation Chain with context"""
+    
     context = f"""
         Context: You are a receptionist calling {patient_name} to schedule an appointment because it's been at least one year.
-        To schedule the appointment, you need to receive the patient's full name, their insurance, and the day they are available to have an appointment.
-        Greet them like a receptionist before retrieving any information. Only ask one question at a time. Keep responses concise but stay friendly.
-        End the conversation once you've received all the information you need.
+        To schedule the appointment, you need to confirm the patient's full name and their insurance, then find a day they are available to have an appointment.
+        End the conversation once you've received all the information you need and explicitly say "bye".
+
+        Greet them like a receptionist before retrieving any information. Only ask one question at a time. Use three sentences maximum and keep responses concise and friendly.
+        If you don't know an answer, just say that you don't know but always check the patient information before saying you don't know. 
         
         You can use the doctor's schedule for reference but never share the doctor's schedule. The following is the doctor's schedule: 
         <doctor_schedule>
@@ -42,6 +45,15 @@ def start_conversation(llm, patient_name) -> None:
             Friday: Busy
             Weekend: Closed
         </doctor_schedule>
+    """
+    
+    retriever = vectorstore.as_retriever()
+    rag_documents = helper.format_docs(retriever.invoke(context))
+    rag_context = f"""
+        The following is information related to the patient:
+        <patient_information>
+            {rag_documents}
+        <patient_information>
     """
 
     prompt = PromptTemplate(
@@ -61,6 +73,9 @@ def start_conversation(llm, patient_name) -> None:
     memory = ConversationBufferMemory(ai_prefix="Assistant")
     memory.chat_memory.add_user_message(context)
     memory.chat_memory.add_ai_message("I am a receptionist calling to schedule an appointment")
+    if rag_documents:
+        memory.chat_memory.add_user_message(rag_context)
+        memory.chat_memory.add_ai_message("This is information relating to this patient")
 
     conversation = ConversationChain(
         prompt=prompt,
@@ -73,11 +88,10 @@ def start_conversation(llm, patient_name) -> None:
     response = conversation.predict(input="Hello")
     print(f"Assistant:{response}")
     
-    user_input = input("Human: ").strip()
-    while user_input.lower() != 'q':
+    while "bye" not in response and "Bye" not in response:
+        user_input = input("Human: ").strip()
         response = conversation.predict(input=user_input)
         print(f"Assistant:{response}")
-        user_input = input("Human: ").strip()
 
 
 
@@ -87,7 +101,9 @@ bedrock_client = boto3.client(
 )
 
 llm = configure_model(bedrock_client)
+vectorstore = helper.generate_embeddings(bedrock_client)
 
-patients_to_call = helper.CheckAppointmentNeeded()
+patients_to_call = helper.check_appointment_needed()
 for patient in patients_to_call:
+    print("\n\n")
     start_conversation(llm, patient)
--- helper.py
+++ helper.py
@@ -1,8 +1,11 @@
 import json
 import random
 from datetime import datetime
+from langchain_chroma import Chroma
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain_text_splitters import RecursiveJsonSplitter
 
-def CreateJSON() -> None:
+def create_JSON() -> None:
     """ Creates a JSON file filled with random patient data """
     patient_data = {}
     patient_names = ["Laura Diaz", "Rose Jackson", "Julia Robinson", "Patrick Gray"]
@@ -11,7 +14,7 @@ def CreateJSON() -> None:
     for patient_name in patient_names:
         patient = {}
         patient["name"] = patient_name
-        patient["age"] = random.randrange(18, 91)
+        patient["age"] = str(random.randrange(18, 91))
         patient["phone"] = f"{random.randrange(100, 1000)}-{random.randrange(0, 1000):03}-{random.randrange(0, 10000):04}"
         patient["insurance"] = random.choice(insurance_providers)
         patient["last_appointment"] = f"{random.randrange(2020, 2024)}-{random.randrange(1, 12):02}-{random.randrange(1, 30):02}"
@@ -22,7 +25,7 @@ def CreateJSON() -> None:
     with open('data.json', 'w') as data_file:
         json.dump(patient_data, data_file)
 
-def CheckAppointmentNeeded() -> list[str]:
+def check_appointment_needed() -> list[str]:
     """ Returns a list of patient names that need a routine checkup """
     output = []
 
@@ -37,6 +40,24 @@ def CheckAppointmentNeeded() -> list[str]:
 
     return output
 
+def generate_embeddings(bedrock_client) -> Chroma:
+    """ Converts JSON data into queryable vectorstore for RAG """
+    with open('data.json', 'r') as data_file:
+        patient_data = json.load(data_file)
 
+    splitter = RecursiveJsonSplitter(max_chunk_size=300)
+    docs = splitter.create_documents(texts=[patient_data])
 
-CreateJSON()
+    vectorstore = Chroma.from_documents(
+        documents=docs,
+        embedding=BedrockEmbeddings(client=bedrock_client)
+    )
+
+    return vectorstore
+
+def format_docs(docs) -> str:
+    # return "\n".join(doc.page_content for doc in docs)
+    # Scalable Solution ^
+    return docs[0].page_content
+
+# create_json()
